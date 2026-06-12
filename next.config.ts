903cd564d67a1ce9cb5e10aa96157335e05a7b36
Anay Shah <anayshah29@gmail.com>
2026-06-11 21:20:44 -0400

    include prisma binary in vercel file tracing

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@prisma/client", "@prisma/client/runtime/library", ".prisma/client", "@prisma/engines"],
+  outputFileTracingIncludes: {
+    "**": ["./src/generated/prisma/**/*"],
+  },
   // devIndicators: { appIsrStatus: true, buildActivity: true, buildActivityPosition: "bottom-right" },
   devIndicators: false,
 };
